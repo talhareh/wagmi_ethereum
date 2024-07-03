dc1d0ed7e100faefb586d6df92be21d4dc2bb2d4
talhareh <talha59@gmail.com>
2024-07-03 15:42:01 +0400

    sb sy aakhri changes

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -7,7 +7,8 @@ let initialState = {
   account: null,
   chainIdArray: [],
   chainId: 0,
-  balance:null
+  bnbBalance:null,
+  usdtBalance:null
 };
 
 export const AppContext = createContext(initialState);
@@ -15,20 +16,28 @@ export const AppContext = createContext(initialState);
 export const ConectivityProvider = ({ children }) => {
   const { address, isDisconnected } = useAccount();
   const { chains, chain } = useNetwork();
-  
-
+  const { data: usdtData} = useBalance({
+                                            address,
+                                            token: '0x55d398326f99059fF775485246999027B3197955',
+                                            chainId:56
+                                          })
+  //console.log(usdtBalance)
+  const {data: bnbData} =useBalance({address})
+  //console.log(bnbBalance)
   const chainIds = chains?.map((info) => info?.id);
-  
-
   const [state, setState] = useState(initialState);
   const [adminAddress, setAdminAddress] = useState("");
 
 
   useEffect(() => {
-    setState({
+    setState((prevState) => ({
+      ...prevState,
       account: address ?? null,
-    });
-  }, [isDisconnected, address]);
+      bnbBalance: bnbData?.formatted ?? null,
+      usdtBalance: usdtData?.formatted ?? null
+    }));
+  }, [isDisconnected, address, bnbData, usdtData]);
+
   
   return (
     <AppContext.Provider
@@ -38,6 +47,8 @@ export const ConectivityProvider = ({ children }) => {
         chainId: chain?.id,
         adminAddress,
         setAdminAddress,
+        bnbBalance: state.bnbBalance,
+        usdtBalance: state.usdtBalance
         
         
       }}
