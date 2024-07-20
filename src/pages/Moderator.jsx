123e2d291edcda27392b8c785757190c6062ee26
talhareh <talha59@gmail.com>
2024-07-20 21:11:25 +0400

    user stats issue resolved

--- src/pages/Moderator.jsx
+++ src/pages/Moderator.jsx
@@ -1,16 +1,12 @@
-import { Box, Button, Container, Grid, Stack, TextField, Typography,
-         Table, TableBody, TableCell, TableContainer, TableHead, 
-         TableRow,Paper
- } from "@mui/material";
+import { Box, Button, Container, Grid, Stack, TextField, Typography,LinearProgress, Divider
+       } from "@mui/material";
 import { useEffect, useState } from "react";
 import { ToastNotify } from "../components/SmallComponents/AppComponents";
 import {  fetchMetrics } from "../ConnectivityAssets/hooks";
-import IncUSDRaised from "../components/SmallComponents/IncUSDRaised";
-import AddTokensSold from "../components/SmallComponents/AddTokensSold";
-import axios from 'axios'
-import ProgIncr from "../components/SmallComponents/ProgIncr";
-import TargetStage from "../components/SmallComponents/TargetStage";
 
+import axios from 'axios'
+
+import './mod.css'
 
 
 const gridItemStyle = {
@@ -36,24 +32,26 @@ const btnStyle = {
   },
 };
 function Moderator() {
-  
-  const [openUsd, setOpenUsd] = useState(false)
+
   const [usdRaised, setUsdRaised] = useState(null)
-  const [openSoldTok, setOpenSoldTok] = useState(false)
   const [soldTok, setSoldTok] = useState(null)
-  const [openProg, setOpenProg] = useState(false)
+ 
   const [prog, setProg] = useState(null)
   const [target, setTarget] = useState(null)
-  const [openTar, setOpenTar] = useState(false)
+
   const [metrics, setMetrics] = useState({})
+  const [calcUsd, setCalcUsd] = useState(0)
+  const [calcTok, setCalcTok] = useState(0)
+  const [esProg, calcEsProg] = useState(0)
   const [load, setLoad] = useState(false)
-  
+  let price = 0.0000246
 
   const [alertState, setAlertState] = useState({
     open: false,
     message: "",
     severity: undefined,
   });
+
   const showAlert = (message, severity = "error") => {
     setAlertState({
       open: true,
@@ -91,259 +89,250 @@ function Moderator() {
       let met = await fetchMetrics()
       setMetrics(met[0])
     }
-
     getData()
   },[])
   
-
   return (
     <Box>
       <ToastNotify alertState={alertState} setAlertState={setAlertState} />
-      
-      <IncUSDRaised
-        open={openUsd}
-        setOpen = {setOpenUsd}
-        usdRaised={usdRaised}
-        setUsdRaised={setUsdRaised}
-        USDHandler= {updateStat}
-      />
-
-      <AddTokensSold
-        open={openSoldTok}
-        setOpen = {setOpenSoldTok}
-        soldToken={soldTok}
-        setSoldToken={setSoldTok}
-        soldTokHandler= {updateStat}
-      />
-
-      <ProgIncr
-        open={openProg}
-        setOpen = {setOpenProg}
-        progIncr={prog}
-        setProgIncr={setProg}
-        ProgIncrHandler= {updateStat}
-      />
-
-      <TargetStage
-        open= {openTar}
-        setOpen = {setOpenTar}
-        target = {target}
-        setTarget={setTarget}
-        targetHandler={updateStat}
-      />
-      
-      <Container maxWidth="lg">        
-        
-        <Grid 
-          item 
-          xs={6} 
-          sm={6} 
-          pl={{ xs: 1 }}
-          sx={{display:"flex"}}
-        >
-            <Typography
-                sx={{
-                    fontFamily: "ProductSansRegular",
-                    fontSize: { xs: "10px", sm: "16px" },
-                    fontWeight:600,
-                    marginTop:"30px",
-                    color: '#F8922A',
-                    textTransform: "uppercase",
-                  }}
-            >
-                Updated Stats 
-            </Typography>
-            
-            <Box
-              sx={{
-                backgroundColor: "#889de1",
-                borderRadius: "10px",
-                p: { xs: 1, sm: 2 },
-                margin:"5px"
-              }}
-            >
-              <Typography
-                sx={{
-                  fontFamily: "ProductSansRegular",
-                  fontSize: { xs: "10px", sm: "12px" },
-                  color: "#fff",
-                  textTransform: "uppercase",
-                }}
-              >
-                Usd Raised
-              </Typography>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <Typography
+        <Container maxWidth="lg">
+          <Grid container spacing={2} justifyContent="center">
+            <Grid 
+            item 
+            xs={6} 
+            sm={6} 
+            pl={{ xs: 1 }}
+            sx={{display:"flex"}}
+            > 
+                <Box
                   sx={{
-                    fontFamily: "ProductSansRegular",
-                    fontSize: { xs: "15px", sm: "20px" },
-                    color: "#fff",
-                    wordBreak: "break-all",
+                    backgroundColor: "#889de1",
+                    borderRadius: "10px",
+                    p: { xs: 1, sm: 2 },
+                    margin:"5px"
                   }}
                 >
-                  {metrics.usr_raised}
-                </Typography>
-                 
-              </Box>
-            </Box>
-
-            <Box
-              sx={{
-                backgroundColor: "#889de1",
-                borderRadius: "10px",
-                p: { xs: 1, sm: 2 },
-                margin:"5px"
-              }}
-            >
-              <Typography
-                sx={{
-                  fontFamily: "ProductSansRegular",
-                  fontSize: { xs: "10px", sm: "12px" },
-                  color: "#fff",
-                  textTransform: "uppercase",
-                }}
-              >
-                Tokens Sold
-              </Typography>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <Typography
-                  sx={{
-                    fontFamily: "ProductSansRegular",
-                    fontSize: { xs: "15px", sm: "20px" },
-                    color: "#fff",
-                    wordBreak: "break-all",
-                  }}
-                >
-                  {metrics.views_taken}
-                </Typography>
-                 
-              </Box>
-            </Box>
-
-            <Box
-              sx={{
-                backgroundColor: "#889de1",
-                borderRadius: "10px",
-                p: { xs: 1, sm: 2 },
-                margin:"5px"
-              }}
-            >
-              <Typography
-                sx={{
-                  fontFamily: "ProductSansRegular",
-                  fontSize: { xs: "10px", sm: "12px" },
-                  color: "#fff",
-                  textTransform: "uppercase",
-                }}
-              >
-                Progress Bar
-              </Typography>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <Typography
-                  sx={{
-                    fontFamily: "ProductSansRegular",
-                    fontSize: { xs: "15px", sm: "20px" },
-                    color: "#fff",
-                    wordBreak: "break-all",
-                  }}
-                >
-                  {metrics.average}
-                </Typography>
-                 
-              </Box>
-            </Box>
-
-            <Box
-              sx={{
-                backgroundColor: "#889de1",
-                borderRadius: "10px",
-                p: { xs: 1, sm: 2 },
-                margin:"5px"
-              }}
-            >
-              <Typography
-                sx={{
-                  fontFamily: "ProductSansRegular",
-                  fontSize: { xs: "10px", sm: "12px" },
-                  color: "#fff",
-                  textTransform: "uppercase",
-                }}
-              >
-                Presale Target
-              </Typography>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <Typography
-                  sx={{
-                    fontFamily: "ProductSansRegular",
-                    fontSize: { xs: "15px", sm: "20px" },
-                    color: "#fff",
-                    wordBreak: "break-all",
-                  }}
-                >
-                  {metrics.usr_target}
-                </Typography>
-                 
-              </Box>
-            </Box>
-
-            
-        </Grid> 
-        
-        <Stack
-          sx={{
-            
-            my: 3,
-            py: 4,
-            borderRadius: "12px",
-          }}
-        >
-          <Grid container spacing={2} justifyContent={""}>
-            <Grid item xs={12} sm={6} md={3} sx={gridItemStyle}>
-              <Box>
-                <TextField
+                  <Typography
                     sx={{
-                        marginBottom:'8px',
-                        backgroundColor:"#ffff"
+                      fontFamily: "ProductSansRegular",
+                      fontSize: { xs: "10px", sm: "12px" },
+                      color: "#fff",
+                      textTransform: "uppercase",
                     }}
+                  >
+                    Usd Raised
+                  </Typography>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Typography
+                      sx={{
+                        fontFamily: "ProductSansRegular",
+                        fontSize: { xs: "15px", sm: "20px" },
+                        color: "#fff",
+                        wordBreak: "break-all",
+                      }}
+                    >
+                      {metrics.usr_raised}
+                    </Typography>
+                    
+                  </Box>
+                </Box>
+
+                <Box
+                  sx={{
+                    backgroundColor: "#889de1",
+                    borderRadius: "10px",
+                    p: { xs: 1, sm: 2 },
+                    margin:"5px"
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      fontFamily: "ProductSansRegular",
+                      fontSize: { xs: "10px", sm: "12px" },
+                      color: "#fff",
+                      textTransform: "uppercase",
+                    }}
+                  >
+                    Tokens Sold
+                  </Typography>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Typography
+                      sx={{
+                        fontFamily: "ProductSansRegular",
+                        fontSize: { xs: "15px", sm: "20px" },
+                        color: "#fff",
+                        wordBreak: "break-all",
+                      }}
+                    >
+                      {metrics.views_taken}
+                    </Typography>
+                    
+                  </Box>
+                </Box>
+
+                <Box
+                  sx={{
+                    backgroundColor: "#889de1",
+                    borderRadius: "10px",
+                    p: { xs: 1, sm: 2 },
+                    margin:"5px"
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      fontFamily: "ProductSansRegular",
+                      fontSize: { xs: "10px", sm: "12px" },
+                      color: "#fff",
+                      textTransform: "uppercase",
+                    }}
+                  >
+                    Progress Bar
+                  </Typography>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Typography
+                      sx={{
+                        fontFamily: "ProductSansRegular",
+                        fontSize: { xs: "15px", sm: "20px" },
+                        color: "#fff",
+                        wordBreak: "break-all",
+                      }}
+                    >
+                      {metrics.average}
+                    </Typography>
+                    
+                  </Box>
+                </Box>
+
+                <Box
+                  sx={{
+                    backgroundColor: "#889de1",
+                    borderRadius: "10px",
+                    p: { xs: 1, sm: 2 },
+                    margin:"5px"
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      fontFamily: "ProductSansRegular",
+                      fontSize: { xs: "10px", sm: "12px" },
+                      color: "#fff",
+                      textTransform: "uppercase",
+                    }}
+                  >
+                    Presale Target
+                  </Typography>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Typography
+                      sx={{
+                        fontFamily: "ProductSansRegular",
+                        fontSize: { xs: "15px", sm: "20px" },
+                        color: "#fff",
+                        wordBreak: "break-all",
+                      }}
+                    >
+                      {metrics.usr_target}
+                    </Typography>
+                    
+                  </Box>
+                </Box>    
+            </Grid> 
+          </Grid>
+            {/* Start of second Row*/ }
+            
+            
+            <Grid item xs={12} sm={6} md={3}>
+              <Box 
+              sx={{
+                    display: 'flex',
+                    flexDirection: 'column', 
+                     
+                  }}>
+                <TextField
+                  sx={{
+                    marginBottom: '8px',
+                    backgroundColor: "#ffff"
+                  }}
                 />
                 <Button 
-                    sx={btnStyle}
-                    onClick={checkingRate}
+                  sx={btnStyle}
+                  onClick={checkingRate}
                 >
-                Check
-              </Button>
+                  Check
+                </Button>
               </Box>
-
-              
             </Grid>
             
-          </Grid>
-        </Stack>
-      </Container>
+            <Grid item xs={12}>
+              <Stack
+                sx={{
+                  my: 3,
+                  py: 4,
+                  borderRadius: "12px",
+                  backgroundColor: "#f5f5f5",
+                  padding: 2
+                }}
+              >
+                <Grid container spacing={2}>
+                  <Grid item xs={4}>
+                    <Typography sx={{ fontWeight: 600 }}>USD</Typography>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Typography sx={{ fontWeight: 600 }}>Tokens</Typography>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Typography sx={{ fontWeight: 600 }}>Progress Bar</Typography>
+                  </Grid>
+
+                  <Grid item xs={4}>
+                    <Typography>{calcUsd}</Typography>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Typography>{calcTok}</Typography>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Box sx={{ width: '100%' }}>
+                      <Typography
+                        sx={{
+                          height: 10,
+                          borderRadius: 5,
+                        }}
+                      >
+                        {esProg}
+                      </Typography>
+                    </Box>
+                  </Grid>
+                </Grid>
+              </Stack>
+            </Grid>
+            
+          
+          
+        </Container>
+      
     </Box>
   );
 }
